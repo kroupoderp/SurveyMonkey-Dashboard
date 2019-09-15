f3919ff39461cc406a2bbfbf06698bb69355579a
Philip Kroupoderov <philipkroupoderov@Philips-MacBook-Pro.local>
2019-09-15 10:42:43 -0400

    Added a query to fetch the survey titles.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests, json
 
 
 access_token = 'xGXDyPakeNjQedIC4BTUQcgjknJ-MK-s.0x0Ap51iGOXnNHDjYPGDTkt6PSEycjqvlNEspmq0s2gbzkMXQnD.4n2AnQ9G2DsYjhaiFFYMCJcCM.87kITTBft8YHHCfOi'
-url = "https://api.surveymonkey.com/v3/surveys?title=Canadian%20Vacation%20Preferences"
+url = "https://api.surveymonkey.com/v3/surveys"
 
 
 request_headers = {
@@ -10,7 +10,20 @@ request_headers = {
     'Authorization': f'bearer {access_token}'
 }
 
+def query():
+    d = requests.get(url, headers=request_headers).text
+    json_frmt = json.loads(d)
+    return json_frmt
 
-d = requests.get(url, headers=request_headers).text
-json_frmt = json.loads(d)
-print(json.dumps(json_frmt, indent=2))
+surveys = []
+
+resp = query()
+
+for survey in resp["data"]:
+    surveys.append(survey["title"])
+
+print(surveys)
+
+
+# if __name__ == "__main__":
+#     print(json.dumps(query(), indent=2))

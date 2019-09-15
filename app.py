ec169d9d0e8088dc6fc80a9a829f53b7c144e2ed
Philip Kroupoderov <philipkroupoderov@Philips-MacBook-Pro.local>
2019-09-15 13:59:41 -0400

    Added the route handlers for the surveys that I have so far. For now, they only display the results in json.

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ from flask import Flask, render_template
 import fetch
 
 app = Flask(__name__)
+
+surveys = fetch.getSurveys()
+
 @app.route("/")
-def hello():
-    return render_template("home.html", surveys=fetch.getSurveys())
+def present():
+    return render_template("home.html", surveys=surveys)
+
+@app.route("/results/test")
+def survey_one():
+    return fetch.getResults("188745355")
+
+@app.route("/results/Trends in software development")
+def survey_two():
+    return fetch.getResults("188231210")
+
+@app.route("/results/Canadian vacation preferences")
+def survey_three():
+    return fetch.getResults("167439285")
+
+@app.route("/results/Geography quiz")
+def survey_four():
+    return fetch.getResults("188308498")
--- fetch.py
+++ fetch.py
@@ -3,6 +3,7 @@ import requests, json
 access_token = 'xGXDyPakeNjQedIC4BTUQcgjknJ-MK-s.0x0Ap51iGOXnNHDjYPGDTkt6PSEycjqvlNEspmq0s2gbzkMXQnD.4n2AnQ9G2DsYjhaiFFYMCJcCM.87kITTBft8YHHCfOi'
 url = "https://api.surveymonkey.com/v3/surveys"
 
+
 request_headers = {
     'Content-Type': "application/json",
     'Authorization': f'bearer {access_token}'
@@ -15,10 +16,16 @@ def query():
 
 def getSurveys():
     resp = query()
-    surveys = []
+    surveys = {}
     for survey in resp["data"]:
-        surveys.append(survey["title"])
+        surveys[survey["title"]] = survey["id"]
     return surveys
 
+def getResults(survey_id):
+    d = requests.get(url + f'/{survey_id}/details', headers=request_headers).text
+    json_frmt = json.loads(d)
+    return json_frmt
+
+
 if __name__ == "__main__":
-    getSurveys()
+    print(getSurveys())
